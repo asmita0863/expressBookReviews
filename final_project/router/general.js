ef58ea3aa449d8ff833cc5e36caf67cf300ae470
asmita0863 <asmita0863@gmail.com>
2025-11-23 08:30:01 -0500

    getting the book details based on ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -37,7 +37,16 @@ public_users.get('/title/:title',function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+
+    // Find the book by ISBN
+    const book = Object.values(books).find(b => b.isbn === isbn);
+
+    if (book) {
+        return res.status(200).json({ reviews: book.reviews });
+    } else {
+        return res.status(404).json({ message: "Book not found" });
+    }
 });
 
 module.exports.general = public_users;
