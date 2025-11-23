8e2a20db72b501f0aaa71c434d7fe54c9fdfb731
asmita0863 <asmita0863@gmail.com>
2025-11-23 08:20:30 -0500

    make dataset proper

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,14 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
+      1: {"isbn":"100","author": "Chinua Achebe","title": "Things Fall Apart", "reviews": ["Good book", "Interesting Topic"] },
+      2: {"isbn":"101","author": "Hans Christian Andersen","title": "Fairy tales", "reviews": ["Terrible", "Do not like at all"] },
+      3: {"isbn":"102","author": "Dante Alighieri","title": "The Divine Comedy", "reviews": ["Amazing"] },
+      4: {"isbn":"103","author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": ["Full of mis-information", "Lost in the context"] },
+      5: {"isbn":"104","author": "Unknown","title": "The Book Of Job", "reviews": ["Horrible", "Difficult to make out"] },
+      6: {"isbn":"105","author": "Unknown","title": "One Thousand and One Nights", "reviews": ["Excellent book", "Reflection "] },
+      7: {"isbn":"106","author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": [] },
+      8: {"isbn":"107","author": "Jane Austen","title": "Pride and Prejudice", "reviews": [] },
+      9: {"isbn":"108","author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews":[] },
+      10:{"isbn":"109","author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": [] }
 }
 
 module.exports=books;
