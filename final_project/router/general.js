e2069b7f68aba306b654d4134f26b0d6aee531f4
asmita0863 <asmita0863@gmail.com>
2025-11-23 08:38:16 -0500

    getting the book details based on the author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -25,7 +25,18 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const authorName = req.params.author.toLowerCase();
+
+    // Filter books by matching author (case-insensitive)
+    const matchedBooks = Object.values(books).filter(
+        book => book.author.toLowerCase() === authorName
+    );
+
+    if (matchedBooks.length > 0) {
+        return res.status(200).json({ books: matchedBooks });
+    } else {
+        return res.status(404).json({ message: "No books found for this author" });
+    }
 });
 
 // Get all books based on title
