9a3ddb586b872f11717fd70ea2b4972c1cb6412c
asmita0863 <asmita0863@gmail.com>
2025-12-06 15:49:09 +0530

    getting the book details based on ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -39,19 +39,25 @@ public_users.get('/all-books', (req, res) => {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
+public_users.get('/isbn/:isbn', async function (req, res) {
   const isbn = req.params.isbn;
 
-    // Find the book by ISBN
-    const book = Object.values(books).find(b => b.isbn === isbn);
+  try {
+    const host = req.protocol + '://' + req.get('host');
+    const response = await axios.get(host + '/all-books');
+    const allBooks = response.data;
+
+    const book = Object.values(allBooks).find(b => b.isbn === isbn);
 
     if (book) {
-        return res.status(200).json({ book: book });
+      return res.status(200).json({ book: book });
     } else {
-        return res.status(404).json({ message: "Book not found" });
-    }return res.status(300).json({message: "Yet to be implemented"});
- });
+      return res.status(404).json({ message: "Book not found" });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: 'Error fetching book details', error: err.message });
+  }
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
