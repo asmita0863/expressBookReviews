0248b5ee5012152567bd29de5bf85a82d5550ee6
asmita0863 <asmita0863@gmail.com>
2025-12-06 15:51:34 +0530

    getting the book details based on author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -60,20 +60,26 @@ public_users.get('/isbn/:isbn', async function (req, res) {
 });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
+public_users.get('/author/:author', async function (req, res) {
   const authorName = req.params.author.toLowerCase();
 
-    // Filter books by matching author (case-insensitive)
-    const matchedBooks = Object.values(books).filter(
-        book => book.author.toLowerCase() === authorName
+  try {
+    const host = req.protocol + '://' + req.get('host');
+    const response = await axios.get(host + '/all-books');
+    const allBooks = response.data;
+
+    const matchedBooks = Object.values(allBooks).filter(
+      book => book.author.toLowerCase() === authorName
     );
 
     if (matchedBooks.length > 0) {
-        return res.status(200).json({ books: matchedBooks });
+      return res.status(200).json({ books: matchedBooks });
     } else {
-        return res.status(404).json({ message: "No books found for this author" });
+      return res.status(404).json({ message: "No books found for this author" });
     }
+  } catch (err) {
+    return res.status(500).json({ message: 'Error fetching books by author', error: err.message });
+  }
 });
 
 // Get all books based on title
